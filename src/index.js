946abb9abfddbc0811b90c96907a3f3148c412c7
Shivam <shivamsinghl498@gmail.com>
2021-05-30 01:11:11 +0530

    calculator-files

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Calculator from './components/Calculator';
+
+ReactDOM.render(<Calculator/>, document.getElementById('root'));
